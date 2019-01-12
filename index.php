0e4e4c082ef6514c170b1b339826544bd87eb85b
Jake Coming <jake@d4webdesign.com>
2019-01-12 12:36:30 -0800

    Clean index.php
    Remove unneeded links and remove non-essential whitespace

--- index.php
+++ index.php
@@ -7,15 +7,5 @@ Author: Jake Coming
 Text Domain: affiliate-site-creator
 */
 
-
-// function responsible for creating affiliate blog on multisite
+// Creates a blog for newly approved affiliates
 include('create-affiliate-blog-site.php');
-
-// $new_affiliate_site = shortcode_create_affiliate_blog_site($affiliate_attributes);
-
-// register & configure management tab in back end
-include('affiliatewp-site-tab.php');
-
-
-// Eventually this should email the new affiliate, letting them know their site is ready to start using
-
