0e017f71b5bbf96c88c3a56b1d6fa4aef6f22bab
JakeComing <jake@d4webdesign.com>
2019-01-12 13:54:44 -0800

    Arbitrary commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: AffiliateWP Site Creator
-Description: Creates a new site on a multisite cluster after a new affiliate is approved. 
+Description: Creates a new site on a multisite cluster after a new affiliate is approved.
 Version 0.1
 Author: Jake Coming
 Text Domain: affiliate-site-creator
